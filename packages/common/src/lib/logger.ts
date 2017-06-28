66814d75a9d97ec9cf9c5764b5091b4048b0fb22
Erik Vullings <erik.vullings@gmail.com>
2017-06-28 09:57:58 +0200

    Added error logger to utils

--- packages/common/src/lib/logger.ts
+++ packages/common/src/lib/logger.ts
@@ -3,3 +3,5 @@ export const logger = (enable: boolean, level: 'DEBUG' | 'ERROR' = 'DEBUG') => e
     const log = level === 'ERROR' ? console.error : console.log;
     log(`${process.title}: ${msg}`); }
   : () => undefined;
+
+export const logError = (msg: string | Object) => { if (msg) { logger(true, 'ERROR')(msg); } };
