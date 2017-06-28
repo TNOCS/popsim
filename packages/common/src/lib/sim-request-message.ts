64619797fa22c852ad57130353c3965ecf6bfd4c
Erik Vullings <erik.vullings@gmail.com>
2017-06-28 09:58:19 +0200

    SimRequestMessage: start and end time is a Date

--- packages/common/src/lib/sim-request-message.ts
+++ packages/common/src/lib/sim-request-message.ts
@@ -1,9 +1,3 @@
-export interface ISimTime {
-  hour: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23;
-  min: 0 | 5 | 10 | 15 | 20 | 25 | 30 | 35 | 40 | 45 | 50 | 55 | 60;
-  day: 'mo' | 'tu' | 'we' | 'th' | 'fr' | 'sa' | 'su';
-}
-
 export interface ISimRequestMessage {
   /**
    * Request ID
@@ -18,14 +12,14 @@ export interface ISimRequestMessage {
    * @type {ISimTime}
    * @memberof 'ISimRequestMessage'
    */
-  simulationStartTime: ISimTime;
+  simulationStartTime: Date;
   /**
    * Simulation end time
    *
    * @type {ISimTime}
    * @memberof 'ISimRequestMessage'
    */
-  simulationEndTime: ISimTime;
+  simulationEndTime: Date;
   /**
    * Bounding box in WGS84 (top-left, bottom-right):
    * [longitude, latitude, longitude, latitude]
